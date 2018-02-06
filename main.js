86a3189b992d1e2f95e4f4548782d7c4c9b3dfe4
Shyam Habarakada <shyam.habarakada@gmail.com>
2018-02-05 18:25:33 -0800

    rename session

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const {
 
 const path = require('path')
 const url = require('url')
-const sessionName = 'persist:10kft-menubar-app'
+const sessionName = 'persist:fat-berris-player-app'
 
 let mobileUserAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25'
 
